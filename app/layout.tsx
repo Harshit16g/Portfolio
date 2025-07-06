6dfc016bc235f5cf86dfcde0160736a1702ded0d
HARSHIT <73606353+Harshit16g@users.noreply.github.com>
2025-07-06 09:55:47 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type React from "react"
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata, Viewport } from "next"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
